54b740e89155a607be8c42db54e58df7e41669ea
rookieraa <zaaraalzh@gmail.com>
2024-04-29 02:24:49 +0700

    Menambahkan api Mahasiswa

---
+++ Mahasiswa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul10_1302220024
+{
+    internal class Mahasiswa
+    {
+        public string Nama { get; set; }
+        public string NIM { get; set; }
+
+        public Mahasiswa(string Nama, string NIM)
+        {
+            this.Nama = Nama;
+            this.NIM = NIM;
+        }
+    }
+}
